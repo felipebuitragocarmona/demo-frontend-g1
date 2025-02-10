910d68d0567643db437ab8bf960269dc6593b933
felipebuitragocarmona <felipebuitragocarmona@gmail.com>
2025-02-10 17:57:56 -0500

    mezcla

--- main.py
+++ main.py
@@ -2,8 +2,5 @@ print("Hola mundo")
 print("segundo dia")
 print("sustentación")
 print("cambio para la nube")
-<<<<<<< HEAD
 print("cambio desde el profe")
-=======
 print("Hola soy Estefania")
->>>>>>> 404e80dfb91bc44062cdc0383adfd59f37533ae6
